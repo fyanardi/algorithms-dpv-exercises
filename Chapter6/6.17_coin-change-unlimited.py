5302f9f43bb3cd0b027e2f733c653d51cc219f14
Fredy Yanardi <fyanardi@gmail.com>
2018-09-01 20:31:35 +0800

    A bit of cleanup for Exercise 6.17

--- Chapter6/6.17_coin-change-unlimited.py
+++ Chapter6/6.17_coin-change-unlimited.py
@@ -10,13 +10,14 @@ an O(nv) dynamic-programming algorithm for the following problem.
 def coin_change_unlimited(x, v):
     """
     Returns True if is possible to make change for v using coins of denominations x1...xn, False
+    otherwise
     Args:
         x: array of coins of denominations x1, x2 ... xn
         v: change value
     """
     n = len(x)
 
-    C = [ False for i in range(v+1) ]
+    C = [False for i in range(v+1)]
     # Base case C(0) = True
     C[0] = True
 
@@ -31,17 +32,17 @@ def coin_change_unlimited(x, v):
 
 
 if __name__ == "__main__":
-    x = [ 5, 10 ]
+    x = [5, 10]
     v = 15
     c = coin_change_unlimited(x, v)
     print('x=%s v=%d change: %s' % (x, v, c))
 
-    x = [ 5, 10 ]
+    x = [5, 10]
     v = 12
     c = coin_change_unlimited(x, v)
     print('x=%s v=%d change: %s' % (x, v, c))
 
-    x = [ 2, 10 ]
+    x = [2, 10]
     v = 9
     c = coin_change_unlimited(x, v)
     print('x=%s v=%d change: %s' % (x, v, c))
